b376d67f54266eb569f183cf0772738d13e2c726
martinkapral <martin.kapraal@gmail.com>
2024-05-30 12:14:57 +0200

    change

--- api.js
+++ api.js
@@ -6,7 +6,7 @@ import {
   doc,
   getDocs,
   getDoc,
-} from "firebase/firestore";
+} from "firebase/firestore/lite";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCMK8uIEjE_2Qvk9hfpPmAiOW1ykTbdvAU",
