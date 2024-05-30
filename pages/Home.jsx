0bd96608d8dd47021b27557378f00c65457d7af0
martinkapral <martin.kapraal@gmail.com>
2024-05-30 11:48:18 +0200

    update

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import homeImg from "../assets/images/home-hero.png";
 
 export default function Home() {
   return (
     <div className="home-container">
-      <h1>You got the travel plans, we got the travel vans.</h1>
-      <p>
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
-      <Link to="vans">Find your van</Link>
+      <img src={homeImg} className="home-hero-image" />
+      <div className="about-page-content">
+        <h1>You got the travel plans, we got the travel vans.</h1>
+        <p>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <Link to="vans">Find your van</Link>
+      </div>
     </div>
   );
 }
